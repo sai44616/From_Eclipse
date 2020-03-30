91551ce94427f9fc8faa2a737e38b5c6f6aece16
sai44616 <45616231+sai44616@users.noreply.github.com>
2020-03-30 19:39:54 +0530

    varma added

--- Eclipse_To_GIT/src/main/java/com/sai/Eclipse_To_GIT/HelloWorldController.java
+++ Eclipse_To_GIT/src/main/java/com/sai/Eclipse_To_GIT/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
  @RequestMapping("/helloworld")
  public ModelAndView hello() {
  
-  String helloWorldMessage = "Hello world  SAI kumar";
+  String helloWorldMessage = "Hello world  SAI kumar varma";
   return new ModelAndView("hello", "message", helloWorldMessage);
  }
 }
