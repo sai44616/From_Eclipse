db53a5831aef722ddcbbeef202542c1f6325cf13
Sai <Sai@hi>
2020-04-01 15:56:56 +0530

    update

--- Eclipse_To_GIT/src/main/java/com/sai/Eclipse_To_GIT/HelloWorldController.java
+++ Eclipse_To_GIT/src/main/java/com/sai/Eclipse_To_GIT/HelloWorldController.java
@@ -11,7 +11,7 @@ public class HelloWorldController {
  @RequestMapping("/helloworld")
  public ModelAndView hello() {
  
-  String helloWorldMessage = "Hello world  from Eclipse....updating in git....ram";
+  String helloWorldMessage = "hi baby";
   return new ModelAndView("hello", "message", helloWorldMessage);
  }
 }
